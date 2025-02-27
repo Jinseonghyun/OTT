4fcb34b39a1ed893fa10badeb5347324fe718efb
Jinseonghyun <jinsunghyun9@gmail.com>
2025-02-27 22:28:37 +0900

    feat: api 요청을 효율적으로 하기 위한 파일 생성
    - 의존성 추가로 RestTemplateConfig 에 Bean 설정 가능
    - api 호출 위한 HttpClient
    - access token 을 감싸서 api 호출 위한 TmdbHttpClient

---
+++ ott-adapters/adapter-http/src/main/java/com/backend/ott/tmdb/TmdbMovieListHttpClient.java
@@ -0,0 +1,31 @@
+package com.backend.ott.tmdb;
+
+import com.backend.ott.client.TmdbHttpClient;
+import com.backend.ott.movie.TmdbMoviePort;
+import com.backend.ott.movie.TmdbPageableMovies;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class TmdbMovieListHttpClient implements TmdbMoviePort {
+
+    @Value("${tmdb.api.movie-lists.now-playing}")
+    private String nowPlayingUrl;
+
+    private final TmdbHttpClient tmdbHttpClient;
+
+    @Override
+    public TmdbPageableMovies fetchPageable(int page) {
+        String url = nowPlayingUrl + "?language=ko-KR&page=" + page;
+        String request = tmdbHttpClient.request(url, HttpMethod.GET, CollectionUtils.toMultiValueMap(Map.of()), Map.of());
+
+
+        return null;
+    }
+}
