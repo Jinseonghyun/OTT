df12cadd6057dbf70acf993fc10235a88feec7f8
Jinseonghyun <jinsunghyun9@gmail.com>
2025-02-27 23:48:19 +0900

    feat: ObjectMapper 사용하여서 객체로 변환

--- ott-adapters/adapter-http/src/main/java/com/backend/ott/tmdb/TmdbMovieListHttpClient.java
+++ ott-adapters/adapter-http/src/main/java/com/backend/ott/tmdb/TmdbMovieListHttpClient.java
@@ -1,8 +1,11 @@
 package com.backend.ott.tmdb;
 
 import com.backend.ott.client.TmdbHttpClient;
+import com.backend.ott.movie.TmdbMovie;
 import com.backend.ott.movie.TmdbMoviePort;
 import com.backend.ott.movie.TmdbPageableMovies;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -25,7 +28,23 @@ public class TmdbMovieListHttpClient implements TmdbMoviePort {
         String url = nowPlayingUrl + "?language=ko-KR&page=" + page;
         String request = tmdbHttpClient.request(url, HttpMethod.GET, CollectionUtils.toMultiValueMap(Map.of()), Map.of());
 
+        TmdbMovieNowPlayingResponse response;
+        try {
+            response = new ObjectMapper().readValue(request, TmdbMovieNowPlayingResponse.class);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
 
-        return null;
+        return new TmdbPageableMovies(
+                response.getResults().stream()
+                        .map(movie -> new TmdbMovie(
+                                movie.getTitle(),
+                                movie.getAdult(),
+                                movie.getGenreIds(),
+                                movie.getOverview(),
+                                movie.getReleaseDate())).toList(),
+                page,
+                response.getTotalPages() - page != 0 // 조회 하고자 하는 페이지 0 이면 다음 페이지가 없다.
+        );
     }
 }
