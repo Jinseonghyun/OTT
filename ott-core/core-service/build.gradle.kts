8b91e2e2996aeec0782d7b6320efabead3450bf0
Jinseonghyun <jinsunghyun9@gmail.com>
2025-02-28 21:02:26 +0900

    feat: service 모듈에서 persistence 모듈 볼 수 있도록 의존성 추가

--- ott-core/core-service/build.gradle.kts
+++ ott-core/core-service/build.gradle.kts
@@ -3,6 +3,7 @@ dependencies {
     implementation(project(":ott-core:core-port"))
 
     runtimeOnly(project(":ott-adapters:adapter-http"))
+    runtimeOnly(project(":ott-adapters:adapter-persistence"))
 
     implementation("org.springframework:spring-context")
 }
