b9376775cd2a0c36e9194ba480ca95c04ef97b1f
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-01 00:33:30 +0900

    feat: QueryDSL 설정

---
+++ ott-adapters/adapter-persistence/src/main/java/com/backend/ott/repository/SampleCustomRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.backend.ott.repository;
+
+import com.backend.ott.entity.QSampleEntity;
+import com.backend.ott.entity.SampleEntity;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class SampleCustomRepositoryImpl implements SampleCustomRepository {
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public List<SampleEntity> findAll() {
+        return jpaQueryFactory.selectFrom(QSampleEntity.sampleEntity)
+                .fetch();
+    }
+}
