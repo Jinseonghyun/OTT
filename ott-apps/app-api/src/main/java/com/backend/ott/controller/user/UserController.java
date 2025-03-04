6f731050711ca387c83c23c4d5e91b0019fbb881
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-04 22:24:33 +0900

    feat: 소셜로그인 로그인 시 토큰 발급하기

--- ott-apps/app-api/src/main/java/com/backend/ott/controller/user/UserController.java
+++ ott-apps/app-api/src/main/java/com/backend/ott/controller/user/UserController.java
@@ -5,6 +5,7 @@ import com.backend.ott.controller.user.request.UserLoginRequest;
 import com.backend.ott.controller.user.request.UserRegisterRequest;
 import com.backend.ott.security.OttAuthUser;
 import com.backend.ott.token.FetchTokenUseCase;
+import com.backend.ott.token.UpdateTokenUseCase;
 import com.backend.ott.user.FetchUserUseCase;
 import com.backend.ott.user.RegisterUserUseCase;
 import com.backend.ott.user.command.UserRegistrationCommand;
@@ -14,6 +15,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,6 +30,7 @@ public class UserController {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final FetchTokenUseCase fetchTokenUseCase;
     private final FetchUserUseCase fetchUserUseCase;
+    private final UpdateTokenUseCase updateTokenUseCase;
 
     @PostMapping("/api/v1/user/register")
     public OttApiResponse<UserRegistrationResponse> register(@RequestBody UserRegisterRequest request) {
@@ -63,6 +66,19 @@ public class UserController {
         String accessTokenFromKakao = fetchTokenUseCase.getTokenFromKakao(code);
         UserResponse kakaoUser = fetchUserUseCase.findKakaoUser(accessTokenFromKakao);
 
-        return OttApiResponse.ok(null);
+        // 소셜 사용자가 이미 존재하는지 확인을 해야 하고
+        UserResponse byProviderId = fetchUserUseCase.findByProviderId(kakaoUser.getProviderId());
+
+        // 만약 존재하지 않으면, 회원가입을 하는 부분
+        if (ObjectUtils.isEmpty(byProviderId)) {
+            registerUserUseCase.registerSocialUser(
+                    kakaoUser.getUsername(),
+                    kakaoUser.getProvider(),
+                    kakaoUser.getProviderId()
+            );
+        }
+
+        // 토큰을 발급해서 반환
+        return OttApiResponse.ok(updateTokenUseCase.upsertToken(kakaoUser.getProviderId()));
     }
 }
