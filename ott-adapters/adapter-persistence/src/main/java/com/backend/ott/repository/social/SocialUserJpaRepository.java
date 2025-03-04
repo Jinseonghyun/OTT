834d9003247998bd89bdab6639fc9dc44d7d334a
Jinseonghyun <jinsunghyun9@gmail.com>
2025-03-04 22:02:22 +0900

    feat: 소셜사용자 Entity 및 Repository 생성하기

---
+++ ott-adapters/adapter-persistence/src/main/java/com/backend/ott/repository/social/SocialUserJpaRepository.java
@@ -0,0 +1,10 @@
+package com.backend.ott.repository.social;
+
+import com.backend.ott.entity.user.SocialUserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface SocialUserJpaRepository extends JpaRepository<SocialUserEntity, String> {
+    Optional<SocialUserEntity> findByProviderId(String providerId);
+}
